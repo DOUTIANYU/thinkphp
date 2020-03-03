0a23e7908e2f6a8d2c158e8f0c0e65e6ac6a4ef1
1204872800 <1204872800@qq.com>
2020-03-03 15:28:22 +0800

    d

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -12,12 +12,51 @@ class Index
             'secret' => 'f1c242f4f28f735d4687abb469072axx',
             // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
             'response_type' => 'array',
+            'oauth' => [
+                'scopes'   => ['snsapi_userinfo'],
+                'callback' => 'http://gfr.zgmtapp.com/Index/callback',
+            ],
         ];
         $app = Factory::officialAccount($config);
-        $response = $app->oauth->scopes(['snsapi_userinfo'])
-            ->redirect($request->domain());
-        $user = $app->oauth->user();
+        $oauth = $app->oauth;
+
+        // 未登录
+        if (empty($_SESSION['wechat_user'])) {
+
+            //$_SESSION['target_url'] = 'user/profile';
+
+            return $oauth->redirect();
+            // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
+            // $oauth->redirect()->send();
+        }
+
+        // 已经登录过
+        $user = $_SESSION['wechat_user'];
         var_dump($user);
+    }
+    public function callback()
+    {
+        $config = [
+            'app_id' => 'wx3cf0f39249eb0exx',
+            'secret' => 'f1c242f4f28f735d4687abb469072axx',
+            // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
+            'response_type' => 'array',
+            'oauth' => [
+                'scopes'   => ['snsapi_userinfo'],
+                'callback' => 'http://gfr.zgmtapp.com/Index/callback',
+            ],
+        ];
+        $app = Factory::officialAccount($config);
+        $oauth = $app->oauth;
+
+// 获取 OAuth 授权结果用户信息
+        $user = $oauth->user();
+
+        $_SESSION['wechat_user'] = $user->toArray();
+
+        $targetUrl = empty($_SESSION['target_url']) ? '/' : $_SESSION['target_url'];
+
+        header('location:'. $targetUrl); // 跳转到 user/profile
     }
     //获取user信息
     public function getUserSession($code)
